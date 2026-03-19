59db867bdc007d545fac6de7bcb440fe9e1d6340
Paul Goulpié <paul.goulpie@gmail.com>
2026-03-19 08:23:03 +0100

    next step

--- gateway/deps/build.sh
+++ gateway/deps/build.sh
@@ -4,5 +4,14 @@ if [ ! -f cJSON/.git ]; then
 	git submodule init
 	git submodule update cJSON
 fi
+cmake -B build/cJSON cJSON
+cmake --build build/cJSON -j $(nproc)
+sudo cmake --install build/cJSON
 
-cmake --B cJSON/build cJSON
+if [ ! -f sACN/.git ]; then
+	git submodule init
+	git submodule update cJSON
+fi
+cmake -B build/sACN sACN
+cmake --build build/sACN -j $(nproc)
+sudo cmake --install build/sACN
