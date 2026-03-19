8eec858578de295e5022418b59a9b0f7883c4d9b
Paul Goulpié <paul.goulpie@gmail.com>
2026-03-19 14:09:48 +0100

    next step

--- gateway/deps/build.sh
+++ gateway/deps/build.sh
@@ -15,3 +15,11 @@ fi
 cmake -B build/sACN sACN
 cmake --build build/sACN -j $(nproc)
 sudo cmake --install build/sACN
+
+if [ ! -f RF24/.git ]; then
+	git submodule init
+	git submodule update cJSON
+fi
+cmake -B build/RF24 RF24
+cmake --build build/RF24 -j $(nproc)
+sudo cmake --install build/RF24
