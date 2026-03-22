2db7a139f9358a684cb5b6400fbd2687e924a700
Paul Goulpié <paul.goulpie@gmail.com>
2026-03-22 01:30:11 +0100

    next step

--- gateway/deps/cross-build.sh
+++ gateway/deps/cross-build.sh
@@ -4,3 +4,9 @@ CROSS_COMPILE=${CROSS_COMPILE:-aarch64-linux-gnu-}
 
 echo "start with
 	CROSS_COMPILE : ${CROSS_COMPILE}"
+
+export CC=${CROSS_COMPILE}gcc
+export CXX=${CROSS_COMPILE}g++
+export ASM=${CROSS_COMPILE}as
+
+./build.sh
